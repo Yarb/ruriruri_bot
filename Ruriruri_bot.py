f0439a6dc0cea0ed996f36ac8ac71fca9f24be91
Yarb <jstrandman@gmail.com>
2020-11-06 06:51:55 +0200

    Corrected the verification stupidity
    Fixed incorrect calls to verify_chat_id()...thanks idiot me.
    Fixed the verify_chat_id() logic.

--- Ruriruri_bot.py
+++ Ruriruri_bot.py
@@ -102,7 +102,7 @@ def set_state(context: CallbackContext, state: int):
 
 def verify_chat_id(update: Update):
 
-    return update.effective_chat.id == CHAT_ID
+    return str(update.effective_chat.id) == CHAT_ID
 
 
 # Sound playing
@@ -125,20 +125,22 @@ def alert_signal(signum, stack):
 
 def identity(update: Update, context: CallbackContext ):
 
-    if verify_chat_id:
+    if verify_chat_id(update):
         send_message(context, "", MSG_IDENTITY)
 
 
 def respond_to_idiots(update: Update, context: CallbackContext ):
 
-    if verify_chat_id:
-        msg = "@" + update.message.from_user.username
-        send_message(context, msg, MSG_IDIOTS)
+    if verify_chat_id(update):
+        msg = "@" + update.message.from_user.username + " "
+        msg = msg + get_resource(MSG_STUPIDITY)
+        send_message(context, msg, MSG_STUPIDITY)
+
 
     
 def process_report(update: Update, context: CallbackContext ):
 
-    if verify_chat_id:
+    if verify_chat_id(update):
         msg = " ".join(context.args)
         if get_state(context) != CLOSED :
         
@@ -149,14 +151,14 @@ def process_report(update: Update, context: CallbackContext ):
             logging.getLogger().log(35, "New report: " + msg)
             
         else:
-            send_message(context, "", MSG_NOT_OPEN_ERROR)
+            send_message(context, "", MSG_NOT_OPEN)
         if NAUGHTY_RE.match(msg):
             respond_to_idiots(update, context)
             
 
 def give_report(update: Update, context: CallbackContext ):
 
-    if verify_chat_id:
+    if verify_chat_id(update):
         state = get_state(context)
         if  state == REPORTED:
             msg = context.bot_data["status"]
@@ -164,10 +166,22 @@ def give_report(update: Update, context: CallbackContext ):
         elif state == OPEN:
             send_message(context, "", MSG_NO_REPORT)
         else:
-            send_message(context, "", MSG_NOT_OPEN)
+            send_message(context, "", MSG_NOT_OPEN_ERROR)
 
+
+def send_alert(update: Update, context: CallbackContext):
+    
+    if verify_chat_id(update):
+        state = get_state(context)
+        if  state != CLOSED:
+            send_message(context, "", MSG_ALERT)
+            play(get_resource(SOUND_ALERT))
+        else:
+            send_message(context, "", MSG_NOT_OPEN_ERROR)
+    
 
 def activity_check(context: CallbackContext):
+
     if os.path.exists(ACTFILEPATH):
         if get_state(context) == CLOSED:
             set_state(context, OPEN)
@@ -191,12 +205,14 @@ def main():
   
   
     identity_handler = CommandHandler(['who'], identity)
+    alert_handler = CommandHandler(['alert', 'notify'], send_alert)
     report_handler = CommandHandler(['Ruriruri', 'reporting'], process_report)
     status_handler = CommandHandler(['status', 'report'], give_report)
     pervert_and_idiot_handler = MessageHandler(Filters.regex(IDENTITY_RE) & Filters.regex(NAUGHTY_RE), respond_to_idiots)
     
     
     dispatcher.add_handler(identity_handler)
+    dispatcher.add_handler(alert_handler)
     dispatcher.add_handler(report_handler)
     dispatcher.add_handler(status_handler)
     dispatcher.add_handler(pervert_and_idiot_handler)
