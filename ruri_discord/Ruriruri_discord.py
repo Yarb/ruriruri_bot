5391ed80a2dd967212bbe5c794373753e76dab77
Yarb <jstrandman@gmail.com>
2022-12-09 15:09:52 +0200

    Cleanup and some comments.

--- ruri_discord/Ruriruri_discord.py
+++ ruri_discord/Ruriruri_discord.py
@@ -22,6 +22,10 @@ MSG_IDIOTS = "STUPIDITY"
 
 
 class WatchCog(commands.Cog):
+    """Watchdog functions
+    Namely adds the timed watchdog for monitoring the status file changes
+    """
+    
     def __init__(self, bot, activity_file):
         self.bot = bot
         self.target = activity_file
@@ -60,16 +64,20 @@ class WatchCog(commands.Cog):
  
 
 class Messaging(commands.Cog):
+    """Messaging cog additions. Basically functions to send messages"""
+    
     def __init__(self, bot, channel, resources):
         self.bot = bot
         self.channel = channel
         self.resources = resources
 
-    async def post_image_message(self, pic, message): 
+        
+    async def post_image_message(self, pic, message):
         try:
             await self.channel.send(file=discord.File(pic), content=message)
         except FileNotFoundError:
             print("File missing: " + pic)
+    
     
     async def send_msg(self, msg, type):
         try:
@@ -81,9 +89,13 @@ class Messaging(commands.Cog):
                 await self.channel.send(msg)
                 
         except KeyError:
-            print("\n**** Resource configuration error: " + type + " image type missing or resource name mismatch")
+            print(f"\n**** Resource configuration error: {type} image type missing or resource name mismatch")
 
+            
     def get_resource(self, resource):
+        """Randomizes an given resource type from the resource file. 
+        These are basically images and replies for the bot to make it more varied.
+        """
         i = random.randint(0, len(self.resources[resource]) - 1)
         return self.resources[resource][i]
 
@@ -103,33 +115,25 @@ CHANNEL = config["channel"]
 GUILD = config["guild"]
 
 USE_PIC = config["use_pic_type"]
-NAUGHTY_RE = re.compile("|".join(resources["NAUGHTY_REGEX"]))
-IDENTITY_RE = re.compile(resources["IDENTITY_REGEX"])
-    
+NAUGHTY_RE = re.compile("|".join(resources["NAUGHTY_REGEX"]), re.IGNORECASE)
+IDENTITY_RE = re.compile(resources["IDENTITY_REGEX"], re.IGNORECASE)
+
+
+#Initialize the bot
 bot = commands.Bot("$$$$", None, None)
-
-
-
-
 
 @bot.event
 async def on_ready():
+    """When bot activates, set up the guild/channel and register cogs."""
+    
     print('We have logged in as {0.user}'.format(bot))
     guild = discord.utils.get(bot.guilds, name=GUILD)
     channel = discord.utils.get(guild.text_channels, name=CHANNEL)
     if channel:
         bot.add_cog(Messaging(bot, channel, resources))
         bot.add_cog(WatchCog(bot, ACTIVITY_FILE))
-    
-#@bot.event
-#async def on_message(message):
-#    if message.author == bot.user:
-#        return
 
-#    if message.content.startswith('$hello'):
-#        await message.channel.send('Baka!')
-
-
+#Start the bot
 bot.run(TOKEN)
 
 
